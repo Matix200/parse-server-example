5ee0b39b53019413ebdce9a6ce3e0427e36534f8
Matix200 <mateusz.mitek95@gmail.com>
2018-02-04 13:13:46 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+// Example express application adding the parse-server module to expose Parse
+// compatible API routes.
+
 var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
-var path = require('path');
 
-var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
+var databaseUri = process.env.DATABASE_URI || process.env.MONGOLAB_URI;
 
 if (!databaseUri) {
   console.log('DATABASE_URI not specified, falling back to localhost.');
@@ -20,27 +22,17 @@ var api = new ParseServer({
 
 var app = express();
 
-// Serve static assets from the /public folder
-app.use('/public', express.static(path.join(__dirname, '/public')));
-
 // Serve the Parse API on the /parse URL prefix
 var mountPath = process.env.PARSE_MOUNT || '/parse';
 app.use(mountPath, api);
 
 // Parse Server plays nicely with the rest of your web routes
 app.get('/', function(req, res) {
-  res.status(200).send('I dream of being a website.  Please star the parse-server repo on GitHub!');
-});
-
-// There will be a test page available on the /test path of your server url
-// Remove this before launching your app
-app.get('/test', function(req, res) {
-  res.sendFile(path.join(__dirname, '/public/test.html'));
+  res.status(200).send('I dream of being a web site.');
 });
 
 var port = process.env.PORT || 1337;
-var httpServer = require('http').createServer(app);
-httpServer.listen(port, function() {
+app.listen(port, function() {
     console.log('parse-server-example running on port ' + port + '.');
 });
 
