6b90f5591e24156a097de14903a08dc5c5c01c55
Matix200 <mateusz.mitek95@gmail.com>
2018-02-04 13:41:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -16,9 +16,6 @@ var api = new ParseServer({
   appId: process.env.APP_ID || 'myAppId',
   masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'http://localhost:' + port + '/parse',
-  liveQuery: {
-    classNames: ["ForumChanel", "ForumChanelComments", "ForumChanelComments2"] // List of classes to support for query subscriptions
-  }
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
@@ -40,8 +37,3 @@ app.listen(port, function() {
     console.log('parse-server-example running on port ' + port + '.');
 });
 
-let httpServer = require('http').createServer(app);
-httpServer.listen(port);
-var parseLiveQueryServer = ParseServer.createLiveQueryServer(httpServer);
-
-ParseServer.createLiveQueryServer(httpServer);
