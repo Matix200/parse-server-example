3f3660d868cc652b66117bb18b8f5f51fc154832
Matix200 <mateusz.mitek95@gmail.com>
2018-02-05 09:47:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,13 @@ var api = new ParseServer({
   appId: process.env.APP_ID || 'myAppId',
   masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'http://localhost:' + port + '/parse',
+  liveQuery: {
+    classNames: ["ForumChanel", "ForumChanelComments", "ForumChanelComments2"],
+    redisURL: 'redis://localhost:6379' 
+  }
 });
+
+
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
 // javascriptKey, restAPIKey, dotNetKey, clientKey
